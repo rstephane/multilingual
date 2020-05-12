739dd83265a082a2ce3d406aa6171834697bfffd
rstephane <stephane.ribas@gmail.com>
2020-05-12 15:06:43 +0200

    add JS function for menu bugger

--- js/main.js
+++ js/main.js
@@ -137,4 +137,29 @@ var main = {
 
 // 2fc73a3a967e97599c9763d05e564189
 
+// multilingual
+function toggleBurger() {
+	var el = document.getElementById('main-menu');
+	var elBurger = document.getElementById('burger-green');
+	var elOverlay = document.getElementById('overlay');
+
+	var cls = el.classList;
+
+	if (!cls.contains('toggled')) {
+		cls.add('toggled');
+		elOverlay.classList.add('active');
+		elBurger.classList.remove('green');
+	}
+	else {
+		cls.remove('toggled');
+		elOverlay.classList.remove('active');
+		elBurger.classList.add('green');
+	}
+
+	return false;
+}
+
+// end
+
+
 document.addEventListener('DOMContentLoaded', main.init);
